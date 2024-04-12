d8153357a7746be5eaff81544c8aea65d2cb5e0e
Sunny Bapla <98226491+s-bapla@users.noreply.github.com>
2024-04-11 17:32:34 -0700

    added all functionality

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
 function add(num1, num2) {
-  return num1 + num2;
+  return Number(num1) + Number(num2);
 }
 
 function subtract(num1, num2) {
-  return num1 - num2;
+  return Number(num1) - Number(num2);
 }
 
 function multiply(num1, num2) {
-  return num1 * num2;
+  return Number(num1) * Number(num2);
 }
 
 function divide(num1, num2) {
-  return num1 / num2;
+  return Number(num1) / Number(num2);
 }
 
 function operate(operator, first, last) {
   return operator(first, last);
 }
 
-
 const numberButtons = document.querySelectorAll("button.number");
 const numberBtnsArr = Array.from(numberButtons);
 let first, second, operator;
+let flag = false;
 const results = document.querySelector(".results");
 const operatorBtns = document.querySelectorAll(".operator");
 const operatorBtnsArr = Array.from(operatorBtns);
 
-function firstNumber(e) {
+function getFirstNumber(e) {
   if (first === undefined) {
     first = e.target.textContent;
   } else {
@@ -35,40 +35,68 @@ function firstNumber(e) {
   results.textContent = first;
 }
 
+function getSecondNumber(e) {
+  if (second === undefined || flag === true) {
+    second = e.target.textContent;
+  } else {
+    second += e.target.textContent;
+  }
+  results.textContent = second;
+}
+
+function getNumber(e) {
+  if (first === undefined) {
+    getFirstNumber(e);
+  } else {
+    getSecondNumber(e);
+  }
+}
+
+function chooseOperator(e) {
+  if (e.target.textContent === "+") {
+    operator = add;
+  } else if (e.target.textContent === "-") {
+    operator = subtract;
+  } else if (e.target.textContent === "x") {
+    operator = multiply;
+  } else {
+    operator = divide;
+  }
+  first = results.textContent;
+}
 for (let button of numberBtnsArr) {
-  button.addEventListener("click", firstNumber)
-  };
-
-
-for (let button of operatorBtns) {
   button.addEventListener("click", (e) => {
-    if (e.target.textContent === "+") {
-      operator = add;
-    } else if (e.target.textContent === "-") {
-      operator = subtract;
-    } else if (e.target.textContent === "x") {
-      operator = multiply;
-    } else {
-      operator = divide;
+    getNumber(e);
+  });
+}
+
+for (let button of operatorBtnsArr) {
+  button.addEventListener("click", (e) => {
+    if (second !== undefined && flag === false) {
+      results.textContent = operate(operator, first, second);
+      first = results.textContent;
+      second = undefined;
+      flag = false;
     }
-    first = Number(first);
-    for (button of numberBtnsArr) {
-      button.removeEventListener("click", firstNumber);
-      button.addEventListener("click", (e) => {
-        if (second === undefined) {
-          second = e.target.textContent;
-        } else {
-          second += e.target.textContent;
-        }
-        results.textContent = second;
-      });
-    }
+    chooseOperator(e);
   });
 }
 
 const equals = document.querySelector(".equals");
 
-equals.addEventListener("click", () => {
-  second = Number(second);
-  results.textContent = operate(operator, first, second);
+equals.addEventListener("click", (e) => {
+  if (second !== undefined) {
+    results.textContent = operate(operator, first, second);
+    first = results.textContent;
+    flag = true;
+  }
 });
+
+const clear = document.querySelector(".clear");
+
+clear.addEventListener("click", () => {
+  flag = false;
+  first = undefined;
+  second = undefined;
+  results.textContent = "";
+})
